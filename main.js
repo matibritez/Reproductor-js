7a007e503fc2499ae531af2a3ee838a948db5d29
Matias Britez <89950148+matibritez@users.noreply.github.com>
2023-03-26 22:50:34 -0300

    cambios js

--- main.js
+++ main.js
@@ -16,12 +16,30 @@ const songList= [
     }
 ]
 
+let actualSong = null;
+
 //Capturar elementos para trabajar con js
 
 const songs = document.getElementById("songs");
 const audio = document.getElementById("audio");
 const cover = document.getElementById("cover");
 const title = document.getElementById("title");
+const play = document.getElementById("play");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+// Escuchar clicks en el boton play
+
+play.addEventListener("click",() =>{
+    if(audio.paused){
+      playSong()
+    } else{
+      pauseSong()
+    }
+})
+
+next.addEventListener("click", ()=> nextSong())
+prev.addEventListener("prev", ()=> prevSong())
 
 
 //Cargar canciones y mostrar la lista de canciones
@@ -47,10 +65,48 @@ function loadSongs(){
 //Cargar Canción seleccionada
 
 function loadSong(songIndex){
-    audio.src = "./audio/" + songList[songIndex].file
-    audio.play()
-    changeCover(songIndex)
-    changeSongTitle(songIndex)
+    if(songIndex !== actualSong){
+      changeActiveClass(actualSong, songIndex)
+      actualSong = songIndex
+      audio.src = "./audio/" + songList[songIndex].file
+      playSong()
+      changeCover(songIndex)
+    }
+}
+
+//actualizar controles
+function updateControls(){
+    if(audio.paused){
+      play.classList.remove("fa-pause")
+      play.classList.add("fa-play")
+    }else{
+      play.classList.add("fa-pause")
+      play.classList.remove("fa-play")
+    }
+}
+
+//reproducir cancion
+function playSong(){
+  audio.play()
+  updateControls()
+}
+
+//pausar cancion.
+
+function pauseSong(){
+  audio.pause()
+  updateControls()
+}
+
+//cambiar clase activa
+
+function changeActiveClass(lastIndex, newIndex) {
+    const links = document.querySelectorAll("a")
+    if(lastIndex !== null){
+      links[lastIndex].classList.remove("active")
+    }
+    links[newIndex].classList.add("active")
+   
 }
 
 //Cambiar el cover de la cancion
@@ -63,6 +119,24 @@ function changeCover(songIndex){
 //Cambiar el titulo de la cancion
 function changeSongTitle(songIndex){
     title.innerText = songList[songIndex].title
+}
+
+//siguiente cancion
+function nextSong(){
+  if(actualSong < songList.length -1){
+    loadSong(actualSong +1)
+  }else{
+    loadSong(0)
+  }
+}
+
+//anterior cancion
+function prevSong(){
+  if(actualSong > 0){
+    loadSong(actualSong -1)
+  }else{
+    loadSong(songList.length -1)
+  }
 }
 
 
